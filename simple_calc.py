52bdaa4eb66dee17893c64ca373bd491bb9764cf
Ramón Márquez <reimon_4116@hotmail.com>
2020-07-01 21:42:54 -0400

    Changed paradigm to OOP
    Added support for * and / operations, as well as parentheses.

--- simple_calc.py
+++ simple_calc.py
@@ -1,74 +1,151 @@
-def calc_commands(command_input):
-    if command_input == "exit":
-        return True
-    elif command_input == "help":
-        print('''The program calculates the algebraic sum of numbers, 
-including variables''')
-    else:
-        print("Unknown command")
-    return False
+class SmartCalc:
+    def __init__(self):
+        self.variables = {}
+        self.postfix_stack = []
+        self.exit_flag = False
 
-def assign_var(assignment_input, vars_dictionary):
-    try:
-        identifier, assignment = assignment_input.split('=')
-    except ValueError:
-        print("Invalid assignment")
-    else:
-        identifier = identifier.strip()
-        assignment = assignment.strip()
-        if not identifier.isalpha():
-            print("Invalid identifier")
-        elif assignment.isdigit():
-            vars_dictionary[identifier] = assignment
-        elif assignment.isalpha():
-            try:
-                vars_dictionary[identifier] = vars_dictionary[assignment]
-            except KeyError:
-                print("Unknown variable")
+    def exec_command(self, command_input):
+        if command_input == "/exit":
+            self.exit_flag = True
+        elif command_input == "/help":
+            print('''The program calculates the algebraic sum of numbers, 
+                    including variables''')
+        else:
+            print("Unknown command")
+
+    def store_variable(self, assignment_input):
+        if assignment_input.count('=') == 1:
+            identifier, assignment = assignment_input.split('=')
+            identifier = identifier.strip()
+            assignment = assignment.strip()
+            if not identifier.isalpha():
+                print("Invalid identifier")
+            elif assignment.isdigit():
+                self.variables[identifier] = assignment
+            elif assignment.isalpha():
+                if assignment in self.variables:
+                    self.variables[identifier] = self.variables[assignment]
+                else:
+                    print("Unknown variable")
+            else:
+                print("Invalid assignment")
         else:
             print("Invalid assignment")
 
-def simplify_summands(summands, vars_dictionary):
-    simple_summands = []
-    sign = 1
-    for i, summand in enumerate(summands):
-        if summand.isalpha():
-            try:
-                summand = vars_dictionary[summand]
-            except KeyError:
-                simple_summands = None
-                break
-        if summand.lstrip("+-").isdigit():
-            try:
-                simple_summands.append(sign * int(summand))
-            except TypeError:
-                print("Invalid expression")
+    def postfix_stack2ans(self):
+        ans = []
+        for i in self.postfix_stack:
+            if i.isdigit():
+                ans.append(int(i))
             else:
-                sign = 1
-        elif i == len(summands) - 1:
-            print("Invalid expression")
-        elif summand.count("-"):
-            sign *= (-1) ** summand.count("-")
-    return simple_summands
+                a = ans.pop()
+                b = ans.pop()
+                if i == '+':
+                    ans.append(b + a)
+                elif i == '-':
+                    ans.append(b - a)
+                elif i == '*':
+                    ans.append(b * a)
+                elif i == '/':
+                    ans.append(b / a)
+                elif i == '^':
+                    ans.append(b ** a)
+        self.postfix_stack.clear()
+        return ans[0]
 
-dict_vars = {}
-while True:
-    user_input = input().strip()
-    if len(user_input):
-        if user_input.startswith('/'):
-            exit_flag = calc_commands(user_input[1:])
-            if exit_flag:
-                break
-        elif '=' in user_input:
-            assign_var(user_input, dict_vars)
-        elif user_input.isalpha():
-            try:
-                print(dict_vars[user_input])
-            except KeyError:
-                print("Unknown variable")
-        else:
-            try:
-                print(sum(simplify_summands(user_input.split(), dict_vars)))
-            except TypeError:
-                print("Unknown variable")
-print("Bye!")
+    def infix2postfix_stack(self, expression):
+        precedence = {'+': 0, '-': 0, '*': 1, '/': 1, '^': 2}
+        operators = []
+        for i in expression:
+            if i.isdigit():
+                self.postfix_stack.append(i)
+            elif not operators or '(' in (operators[-1], i):
+                operators.append(i)
+            elif i == ')':
+                while operators[-1] != '(':
+                    self.postfix_stack.append(operators.pop())
+                operators.pop()
+            else:
+                while precedence[i] <= precedence[operators[-1]]:
+                    self.postfix_stack.append(operators.pop())
+                    if not operators or operators[-1] == '(':
+                        break
+                operators.append(i)
+        while operators:
+            self.postfix_stack.append(operators.pop())
+
+    def parse_input(self, input_expression):
+        parsed_input = []
+        if input_expression.count('(') != input_expression.count(')'):
+            print("Invalid expression")
+            return parsed_input
+
+        input_expression = input_expression.split()
+        for i in range(len(input_expression) - 1):
+            if (input_expression[i].strip("()").isalnum() and
+                    input_expression[i + 1].strip("()").isalnum()):
+                print("Invalid expression")
+                return parsed_input
+
+        input_expression = "".join(input_expression)
+        for i in input_expression:
+            if parsed_input:
+                if i.isdigit():
+                    if parsed_input[-1].isdigit():
+                        parsed_input.append("".join([parsed_input.pop(), i]))
+                    else:
+                        parsed_input.append(i)
+                elif i.isalpha():
+                    if parsed_input[-1].isalpha():
+                        parsed_input.append("".join([parsed_input.pop(), i]))
+                    else:
+                        parsed_input.append(i)
+                elif i in ('(', ')'):
+                    parsed_input.append(i)
+                else:
+                    if parsed_input[-1].isalnum() or parsed_input[-1] in ('(', ')'):
+                        parsed_input.append(i)
+                    else:
+                        parsed_input.append("".join([parsed_input.pop(), i]))
+            else:
+                parsed_input.append(i)
+
+        for i, chunk in enumerate(parsed_input):
+            if chunk.isalpha():
+                if chunk in self.variables:
+                    parsed_input[i] = self.variables[chunk]
+                else:
+                    parsed_input.clear()
+                    # print(f"Unknown variable: {chunk}")
+                    print("Unknown variable")
+                    break
+            elif not chunk.isdigit() and len(chunk) > 1:
+                if '*' in chunk or '/' in chunk or '^' in chunk:
+                    parsed_input.clear()
+                    print("Invalid expression")
+                    break
+                elif '-' in chunk:
+                    parsed_input[i] = '-' if chunk.count('-') % 2 else "+"
+                elif '+' in chunk:
+                    parsed_input[i] = '+'
+
+        return parsed_input
+
+    def run(self):
+        while not self.exit_flag:
+            user_input = input().strip()
+            if len(user_input):
+                if user_input.startswith('/'):
+                    self.exec_command(user_input)
+                elif '=' in user_input:
+                    self.store_variable(user_input)
+                else:
+                    user_input = self.parse_input(user_input)
+                    if user_input:
+                        self.infix2postfix_stack(user_input)
+                        print(self.postfix_stack2ans())
+        print("Bye!")
+
+
+new_calc = SmartCalc()
+new_calc.run()
